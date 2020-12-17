40db5edf05eb2a2cd345da4cd0911d9a1c10d2be
Sandra <alecsandra.serediuc@gmail.com>
2020-12-17 11:51:12 +0100

    setting the header content type,sending html and handeling routes

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
   
 const http = require('http');
 const port = process.env.PORT || 3000;
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  // console.log(req);
+  console.log(req.url);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+  // routing
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
+
+
 });
 
-// localhost is the default value for 2nd argument
+// localhost is the default value for second argument
 server.listen(port, 'localhost', () => {
     console.log(`Server is listening on ${port}`);
 });
