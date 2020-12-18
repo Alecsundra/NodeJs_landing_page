e259f9b819e06d1a9e1b5b4c984ee1dedb7f8874
Sandra <alecsandra.serediuc@gmail.com>
2020-12-18 15:46:32 +0100

    solved the undifiened word in html

--- public/main.js
+++ public/main.js
@@ -1,23 +1,22 @@
 function myFunction() {
-    var x, result;
-    
+    var x, result='';
     // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
-    console.log(x)
+     console.log(x)
     fetch(`https://api.github.com/users/${x}/repos`)
     .then(function (response) {
         // The API call was successful!
         return response.json();
     }).then(function (data) {
         if (data.length > 0){
-            // This is the JSON from our response, retireving a list and taking only the name pf the repo
+            // This is the JSON from our response, retireving a list and taking only the name of the repo
             data.forEach(function (item) {
                 result += "<li>" + item.full_name.substring(item.full_name.indexOf("/") + 1);
             });
-                console.log(data[0].full_name);
+                // console.log(data[0].full_name);
                 document.getElementById("demo").innerHTML = result;
         } else {
-            document.getElementById("demo").innerHTML = ('Oops, this user do not have any public repos!');
+            document.getElementById("demo").innerHTML = '<span style="color: #d72323">Oops, this user do not have any public repos or do not exist!</span>';
         }
     }).catch(function (err) {
         // There was an error
